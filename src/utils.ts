1c9074c27b762d2633ae8ff46589052af1f5cf7e
merthanmerter <merthanm@gmail.com>
2024-01-25 02:04:27 +0300

    Refactor code structure and add new utility module

---
+++ src/utils.ts
@@ -0,0 +1,7 @@
+export const rsc = {
+	OK: 200,
+	CREATED: 201,
+	BAD_REQUEST: 400,
+	NOT_FOUND: 404,
+	INTERNAL_SERVER_ERROR: 500,
+}
--- tsup.config.ts
+++ tsup.config.ts
@@ -2,7 +2,7 @@ import { defineConfig } from 'tsup'
 
 export default defineConfig({
 	format: ['cjs', 'esm'],
-	entryPoints: ['src/index.ts'],
+	entryPoints: ['src/core.ts', 'src/di.ts', 'src/utils.ts'],
 	dts: true,
 	sourcemap: true,
 	clean: true,
