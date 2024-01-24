6d68ae2e5c4f47b0b7e72e57ca29983c003e9db9
merthanmerter <merthanm@gmail.com>
2024-01-25 01:35:48 +0300

    Add new files and update configurations

---
+++ tsup.config.ts
@@ -0,0 +1,13 @@
+import { defineConfig } from 'tsup'
+
+export default defineConfig({
+	format: ['cjs', 'esm'],
+	entryPoints: ['src/index.ts'],
+	dts: true,
+	sourcemap: true,
+	clean: true,
+	minify: true,
+	splitting: true,
+	shims: true,
+	skipNodeModulesBundle: true,
+})
