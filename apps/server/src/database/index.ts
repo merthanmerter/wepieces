a47e3ea5eee57f4f37e933f429432dd8fb61ad9f
merthanmerter <merthanm@gmail.com>
2024-10-13 20:04:57 +0300

    improve migration

--- apps/server/src/database/index.ts
+++ apps/server/src/database/index.ts
@@ -4,12 +4,18 @@ import postgres from "postgres";
 import { env } from "../../env";
 import * as schema from "./schema";
 
-const pool = postgres({
+export const DATABASE_OPTIONS: postgres.Options<{}> = {
   host: env.DATABASE_HOST,
   port: env.DATABASE_PORT,
   user: env.DATABASE_USER,
   password: env.DATABASE_PASSWORD,
   database: env.DATABASE_SCHEMA,
-});
+  // connect_timeout: 10000,
+  // idle_timeout: 10000,
+  // keep_alive: true,
+  // max: 10,
+  // ssl: true,
+};
 
+const pool = postgres(DATABASE_OPTIONS);
 export const db = drizzle(pool, { schema: schema });
--- apps/server/src/database/migrate.ts
+++ apps/server/src/database/migrate.ts
@@ -1,33 +1,49 @@
 import { migrate as drizzle_migrate } from "drizzle-orm/postgres-js/migrator";
+import postgres from "postgres";
 import { env } from "../../env";
 import { hashPassword } from "../lib/auth";
-import { db } from "./";
+import { DATABASE_OPTIONS, db } from "./";
 import { users } from "./schema";
 
 export const migrate = async () => {
+  /**
+   * 1. Create the `database` if it doesn't exist.
+   */
+  try {
+    await postgres({ ...DATABASE_OPTIONS, database: undefined }).unsafe(
+      `CREATE DATABASE ${env.DATABASE_SCHEMA};`,
+    );
+  } catch (error) {}
+
+  /**
+   * 2. Migrate the database.
+   */
   try {
     await drizzle_migrate(db, {
       migrationsFolder: "drizzle",
     });
-
-    try {
-      await db
-        .insert(users)
-        .values({
-          username: env.SUPERADMIN_USERNAME,
-          email: env.SUPERADMIN_EMAIL,
-          role: "superadmin",
-          password: await hashPassword(env.SUPERADMIN_PASSWORD),
-        })
-        .execute();
-    } catch (error) {}
-
-    console.log("[\x1b[32m✓\x1b[0m] Migration completed");
-    process.exit(0);
   } catch (error) {
     console.error("Error during migration:", error);
     process.exit(1);
   }
+
+  /**
+   * 3. Create the superadmin user if it doesn't exist.
+   */
+  try {
+    await db
+      .insert(users)
+      .values({
+        username: env.SUPERADMIN_USERNAME,
+        email: env.SUPERADMIN_EMAIL,
+        role: "superadmin",
+        password: await hashPassword(env.SUPERADMIN_PASSWORD),
+      })
+      .execute();
+  } catch (error) {}
+
+  console.log("[\x1b[32m✓\x1b[0m] Migration completed");
+  process.exit(0);
 };
 
 migrate();
